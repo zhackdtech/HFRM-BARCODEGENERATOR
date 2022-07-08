9bdf462be42e8dd2eb2747aa9278a9837948d972
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2022-07-08 16:24:36 +0800

    v1.2 added save to jpeg

--- Mainform.Designer.cs
+++ Mainform.Designer.cs
@@ -38,6 +38,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.btnGenerate = new System.Windows.Forms.Button();
             this.pctOutput = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.pnlTitleBar.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnMinimize)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnExit)).BeginInit();
@@ -130,6 +131,7 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "SAVE";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // btnGenerate
             // 
@@ -153,6 +155,14 @@
             this.pctOutput.TabIndex = 5;
             this.pctOutput.TabStop = false;
             // 
+            // panel1
+            // 
+            this.panel1.Enabled = false;
+            this.panel1.Location = new System.Drawing.Point(327, 87);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(118, 120);
+            this.panel1.TabIndex = 6;
+            // 
             // Mainform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -164,6 +174,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.txtInput);
             this.Controls.Add(this.pnlTitleBar);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Mainform";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -190,6 +201,7 @@
         private System.Windows.Forms.Button btnGenerate;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.PictureBox pctOutput;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Mainform.cs
+++ Mainform.cs
@@ -1,10 +1,16 @@
-﻿using HFRM_BARCODEGENERATOR.UI.CustomMessageBoxes;
+﻿using HFRM_BARCODEGENERATOR.Properties;
+using HFRM_BARCODEGENERATOR.UI.CustomMessageBoxes;
 using QRCoder;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -41,14 +47,80 @@ namespace HFRM_BARCODEGENERATOR
             exit.ShowDialog();
 
         }
-
+        Bitmap qrCodeImage;
         private void btnGenerate_Click(object sender, EventArgs e)
         {
             QRCodeGenerator qrGenerator = new QRCodeGenerator();
             QRCodeData qrCodeData = qrGenerator.CreateQrCode(txtInput.Text, QRCodeGenerator.ECCLevel.Q);
             QRCode qrCode = new QRCode(qrCodeData);
-            Bitmap qrCodeImage = qrCode.GetGraphic(4);
+             qrCodeImage = qrCode.GetGraphic(4);
             pctOutput.Image = qrCodeImage;
+
+            var dialog = new SaveFileDialog
+            {
+                Filter = "JPG IMAGES | *.jpg,*.jpeg"
+            };
+            //var userDir = new DirectoryInfo(Environment.GetFolderPath(System.Environment.SpecialFolder.UserProfile));
+            //Directory.CreateDirectory(userDir.FullName+"/Documents/Barcodes/"+filename);
+            //Debug.Write(userDir);
+            dialog.FileName = "dick";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                qrCodeImage.Save(dialog.FileName, ImageFormat.Jpeg);
+            }
         }
+        private void SaveToJpg(String file)
+        {
+            
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveToJpg("dick");
+        }
+        //public static Bitmap CreateBitmap(string text, int height, int width, Color foregroundColor, Color backgroundColor, string fontName, int fontSize, bool antialias)
+        //{
+        //     // Initialize graphics
+        //    Bitmap bmp = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+        //    using (Graphics g = Graphics.FromImage(bmp))
+        //    {
+        //    g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+        //    //g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+        //    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+        //    if (antialias)
+        //    {
+        //        g.SmoothingMode = SmoothingMode.AntiAlias;
+        //        g.TextRenderingHint = TextRenderingHint.AntiAlias;
+        //    }
+
+        //    // Set colors
+        //    SolidBrush fgBrush = new SolidBrush(foregroundColor);
+        //    SolidBrush bgBrush = new SolidBrush(backgroundColor);
+
+        //    // paint background
+        //    RectangleF rectF = new RectangleF(0, 0, width, height);
+        //    g.FillRectangle(bgBrush, rectF);
+
+        //    // Load font
+        //    FontFamily fontFamily = FontFamily.GenericSerif;
+        //    Font font = new Font(fontFamily, fontSize, FontStyle.Regular, GraphicsUnit.Pixel);
+        //    try
+        //    {
+        //        fontFamily = new FontFamily(fontName);
+        //        font = new Font(fontFamily, fontSize, FontStyle.Regular, GraphicsUnit.Pixel);
+        //    }
+        //    catch { }
+
+        //    // Set font direction & alignment
+        //    StringFormat format = new StringFormat();
+        //    format.Alignment = StringAlignment.Center;
+        //    format.LineAlignment = StringAlignment.Center;
+
+        //    // Finally, draw the text
+        //    g.DrawString(text, font, fgBrush, rectF, format);
+
+        //    return bmp;
+        //    }
+        //}
     }
 }
